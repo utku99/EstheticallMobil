826e6cfdaa9e191533b28647192401ce824fc9c4
utku99 <utkuaksoy99@gmail.com>
2024-03-06 17:35:24 +0300

    c

--- App.tsx
+++ App.tsx
@@ -5,13 +5,24 @@ import {
   initialWindowMetrics,
 } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
-import {StatusBar} from 'react-native';
+import {LogBox, StatusBar} from 'react-native';
 import {MD3LightTheme, PaperProvider} from 'react-native-paper';
 import {Provider} from 'react-redux';
 import {persistor, store} from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
 
 function App(): React.JSX.Element {
+  OneSignal.initialize('36ba4e67-6a5f-4bae-9269-4ccdededab2d');
+
+  OneSignal.Notifications.requestPermission(true);
+
+  console.log(OneSignal.User.pushSubscription.getPushSubscriptionId());
+
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <Provider store={store}>
